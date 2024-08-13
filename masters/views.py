aab4aea504de6e8135f4400c985e926380c7dc29
OST <oleh.s.tymchuk@gmail.com>
2024-08-13 19:09:48 +0200

    admin lesson

--- masters/views.py
+++ masters/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Staff
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'staff.html')
+    staff = Staff.objects.filter(is_visible=True).order_by('sort')
+    context = {
+        'staff': staff
+    }
+    return render(request, 'staff.html', context=context)
--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from .models import Category, Dish
+from django.utils.safestring import mark_safe
 
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Dish)
+class DishInline(admin.TabularInline):
+    model = Dish
+    fields = ('name', 'price', 'is_visible', 'sort')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (DishInline, )
+    list_display = ('name', 'is_visible', 'sort')
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('photo_src_tag', 'name', 'price', 'is_visible', 'sort', 'category')
+
+    list_editable = ('price', 'is_visible', 'sort')
+    list_filter = ('category', 'is_visible')
+    search_fields = ('name',)
+
+    def photo_src_tag(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' width=50 height=50>")
+
+    photo_src_tag.short_description = 'Dish photo'
